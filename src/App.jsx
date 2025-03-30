a3d8ca525edf56d77597a6a8be8b06bec3c1e381
Daniel Small <DanielTySmall@ucf.edu>
2025-03-30 15:37:11 -0400

    Text Change

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>DB Project !!</h1>
+      <h1>Programming is My Passion</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
