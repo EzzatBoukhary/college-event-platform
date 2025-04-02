53a7796519b9b9740c01e8be4eb1599a7facb0ed
Daniel Small <DanielTySmall@ucf.edu>
2025-04-02 11:38:34 -0400

    Branch Distinction

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> I Use Emacs btw </h1>
+      <h1> API Branch </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
