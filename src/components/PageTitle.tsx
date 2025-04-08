950628af8eb9b47900ef4b7f83bf28b88e24f9db
Rhynhelt <hib8882@gmail.com>
2025-04-08 16:43:16 -0400

    Update PageTitle.tsx

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -3,6 +3,7 @@ import './PageTitle.css';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom'; // make sure this is imported
 
 interface PageTitleProps {
     loggedIn: boolean;
@@ -22,15 +23,20 @@ function PageTitle({ loggedIn, handleLogout }: PageTitleProps) {
                 <span className="navbar-title">Event Manager</span>
                 {loggedIn && (
                     <div className="ml-auto" style={{padding: 2}}>
-
-                        <Nav className="nav-links">
+                        {/* <Nav className="nav-links">
                             <Nav.Link href="/account-details">Account Details</Nav.Link>
                             <Nav.Link href="/rso-list">RSO List</Nav.Link>
                             <Nav.Link href="/university-profile">University Profile</Nav.Link>
                             <Nav.Link href="/event-list">Event List</Nav.Link>
                             <Button className="logout-button" onClick={handleLogoutClick}>Logout</Button>
+                        </Nav> */}
+                        <Nav className="nav-links">
+                            <Nav.Link as={Link} to="/account-details">Account Details</Nav.Link>
+                            <Nav.Link as={Link} to="/rso-list">RSO List</Nav.Link>
+                            <Nav.Link as={Link} to="/university-profile">University Profile</Nav.Link>
+                            <Nav.Link as={Link} to="/event-list">Event List</Nav.Link>
+                            <Button className="logout-button" onClick={handleLogoutClick}>Logout</Button>
                         </Nav>
-
                     </div>
                 )}
             </div>
