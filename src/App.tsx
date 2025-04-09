cc1ec3ada4070e8951e8eae86695353373c711b3
ezzatBoukhary <ezzat_eagle@hotmail.com>
2025-04-09 19:15:09 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@
   import EventDetailsPage from './pages/EventDetailsPage';
   import CreateRSOPage from './pages/CreateRSOPage';
   import CreateEventPage from './pages/CreateEventPage';
+import PendingEventsPage from './pages/PendingEventsPage';
 
 
   function App() {
